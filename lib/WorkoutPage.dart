e819f3453e1ace40d4910b10e07999079456f1ec
Lim Yu Long <yulonglimbleh@gmail.com>
2021-06-24 15:45:52 +0800

    Changed all ExerciseItem to exerciseData type

--- lib/WorkoutPage.dart
+++ lib/WorkoutPage.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/Exercising.dart';
+import 'package:flutter_app/database/exercise_data.dart';
+import 'package:flutter_app/elements/exercise_card.dart';
 
 class WorkOutPage extends StatefulWidget {
+  final List<exerciseData> items;
+
+  WorkOutPage({Key? key, required this.items}) : super(key: key);
+
   @override
-  _WorkOutPageState createState() => _WorkOutPageState();
+  _WorkOutPageState createState() => _WorkOutPageState(this.items);
 }
 
 class _WorkOutPageState extends State<WorkOutPage> {
+  final listKey = GlobalKey<AnimatedListState>();
+  final List<exerciseData> items;
+  int restDuration = 5;
+
+  _WorkOutPageState(this.items);
+
+  void removeItem(int index) {
+    final removedItem = items[index];
+    items.removeAt(index);
+    listKey.currentState!.removeItem(
+        index,
+            (context, animation) => ExerciseCard(
+          item: removedItem,
+          animation: animation,
+          onClicked: () {},
+        ));
+  }
+
+  String durationMMSS(int duration) {
+    int mins = 0;
+    int temp = duration;
+    while (temp >= 60) {
+      temp -= 60;
+      mins++;
+    }
+    return mins.toString() + 'm ' + temp.toString() + 's';
+  }
+
+  String totalduration() {
+    int duration = 0;
+    for (int counter = 0; counter < items.length; counter++) {
+      duration += items[counter].exerciseTime;
+    }
+    return durationMMSS(duration);
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<exerciseData> copyItems = <exerciseData>[];
+    for (int i = 0; i < items.length; i++) {
+      copyItems.add(items[i]);
+    }
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
@@ -22,9 +69,153 @@ class _WorkOutPageState extends State<WorkOutPage> {
           ),
         ),
         title: Text(
-          " Workout",
+          "Warm Up",
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
         ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.15,
+            child: Container(
+              margin: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Theme.of(context).primaryColor,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'No. of exercises',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                              color: Theme.of(context).secondaryHeaderColor,
+                            ),
+                          ),
+                          Text(
+                            items.length.toString(),
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500,
+                              color: Theme.of(context).secondaryHeaderColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Rest',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500,
+                              color: Theme.of(context).secondaryHeaderColor,
+                            ),
+                          ),
+                          Text(
+                            restDuration.toString() + 's',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500,
+                              color: Theme.of(context).secondaryHeaderColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Duration',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500,
+                              color: Theme.of(context).secondaryHeaderColor,
+                            ),
+                          ),
+                          Text(
+                            totalduration(),
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500,
+                              color: Theme.of(context).secondaryHeaderColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.6,
+            child: AnimatedList(
+              shrinkWrap: true,
+              key: listKey,
+              initialItemCount: items.length,
+              itemBuilder: (context, index, animation) => ExerciseCard(
+                item: items[index],
+                animation: animation,
+                onClicked: () => removeItem(index),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+            width: MediaQuery.of(context).size.width,
+            child: new InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          exercising(items: copyItems, rest: restDuration)),
+                );
+              },
+              child: Container(
+                margin: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Theme.of(context).primaryColor,
+                ),
+                child: Center(
+                  child: Text(
+                    'Start',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                      color: Theme.of(context).secondaryHeaderColor,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
